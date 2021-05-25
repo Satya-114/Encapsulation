303cb0aae1c3028d57d95e71e28ba51233127f88
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-05-25 16:58:51 +0530

    Add files via upload

---
+++ Encapsulation/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Encapsulation_demo
+{ 
+        
+            public class Bank
+        {
+            private double balance;
+            public double Balance
+            {
+                get
+                {
+                    return balance;
+                }
+                set
+                {
+                    // validate the value
+                    if (value < 0)
+                    {
+                        Console.WriteLine("value cannot be negative");
+                    }
+                    balance = value;
+                }
+            }
+        }
+        class BankUser
+        {
+            public static void Main()
+            {
+                Bank SBI = new Bank();
+                SBI.Balance = 100;
+                Console.WriteLine(SBI.Balance);
+                SBI.Balance = -50;
+                Console.WriteLine(SBI.Balance);
+                Console.WriteLine("Press any key to exist.");
+                Console.ReadKey();
+            }
+        }
+    }
+
+
